3f0766049b867466f283ae9f3a5ffcda35844f3a
Suparno Roy Chowdhury <62094534+srchowd3-445@users.noreply.github.com>
2025-04-01 16:05:21 -0700

    Add files via upload

---
+++ src/main/java/inclass/HelloWorld.java
@@ -0,0 +1,14 @@
+package inclass;
+public class HelloWorld {
+
+
+	public static int returnOne() {
+		return 1;
+	}
+	public static void main(String[] args) {
+
+		System.out.println("Hello world");
+	}
+
+
+}
---
+++ src/test/java/ser515/unittest/UnitTest.java
@@ -0,0 +1,19 @@
+package ser515.unittest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import inclass.HelloWorld;
+public class UnitTest {
+
+    @Test
+    void testReturn(){
+        Assertions.assertEquals(1, HelloWorld.returnOne());
+    }
+    @Test
+    void testToHellWorld() {
+        Assertions.assertEquals(1, 1);
+    }
+
+
+
+
+}
